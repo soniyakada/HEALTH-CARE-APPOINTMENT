99a17c415fcfb533a08652e1e1bada07d4773ed2
soniyakada <soniyakada123@gmail.com>
2025-01-12 09:27:31 +0530

    applied validations

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -17,9 +17,66 @@ const Signup = () => {
   const [availability, setAvailability] = useState("");
   const [fees, setFees] = useState("");
   const [loading, setLoading] = useState(false); // New state for loading
-
+  const [error, setError] = useState('');
+  const [emailerror , setEmailerror] = useState('');
+  const [contactError, setContactError] = useState('');
+  const [passError, setPassError] = useState("")
+  const [addError, setAddError] = useState("");
+  const [feeError, setFeeError] = useState("");
+  const [specialError, setSpecialError] = useState("");
+  const [availableError , setAvailabilityError] = useState("");
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+   if(!name.trim()){
+    setError("Please enter your name.");
+    return;
+   }
+   if (!email.trim()) {
+    setEmailerror("Please enter your email.");
+    return;
+  }
+  if (!emailRegex.test(email.trim())) {
+    setEmailerror("Please enter a valid email address.");
+    return;
+  }
+ 
+  const indianContactRegex = /^[6-9]\d{9}$/;
+
+    // Check if the contact number field is empty or invalid
+  if (!contactNumber.trim()) {
+    setContactError("Please enter your contact number.");
+      return;
+  }
+  if (!indianContactRegex.test(contactNumber.trim())) {
+      setContactError("Please enter a valid mobile number.");
+      return;
+  }
+  if (!password.trim()) {
+    setPassError("Password cannot be empty.");
+    return;
+  }
+
+  if(!address.trim()){
+    setAddError("please enter address.");
+    return;
+  }
+
+  if (!specialization.trim()) {
+    setSpecialError("Specialization is required.");
+    return;
+  }
+  if (!availability.trim()) {
+    setAvailabilityError("Availability is required.");
+    return;
+  }
+  if (!fees) {
+    setFeeError("Fees is required.");
+    return;
+  }
+
 
     const formData = {
       name,
@@ -57,7 +114,14 @@ const Signup = () => {
       setSpecialization("");
       setAvailability("");
       setFees("");
-      
+      setAddError("");
+      setAvailabilityError("");
+      setContactError("");
+      setEmailerror("");
+      setError("");
+      setFeeError("");
+      setSpecialError("");
+       
       setLoading(false); // Set loading to false after response
     } catch (error) {
       setLoading(false); // Set loading to false if there's an error
@@ -72,7 +136,7 @@ const Signup = () => {
        <Navbar/>
         <div className="container">
           <form onSubmit={handleSubmit} className="signup-form">
-            <h2>Signup</h2>
+            <h2>Sign up</h2>
 
             {/* Name */}
             <div>
@@ -80,9 +144,12 @@ const Signup = () => {
               <input
                 type="text"
                 value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
+                onChange={(e) => setName(e.target.value)} 
+                onFocus={()=>{
+                  setError("");
+                }}         
               />
+              {error && <span className="text-xs text-red-600">{error}</span>}
             </div>
 
             {/* Email */}
@@ -92,28 +159,42 @@ const Signup = () => {
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                required
+                onFocus={()=>{
+                  setEmailerror("");
+                }}    
               />
-            </div>
-            <div>
-  <label>Contact Number</label>
-  <input
-    type="text"
-    value={contactNumber}
-    onChange={(e) => setContactNumber(e.target.value)}
-    required
-  />
-</div>
+                  {emailerror && <span className="text-xs text-red-600">{emailerror}</span>}
+              </div>
+
+
+          <div>
+          <label>Contact Number</label>
+          <input
+          type="number"
+          maxLength={10}
+          onFocus={()=>{
+            setContactError("");
+          }}  
+          value={contactNumber}
+          onChange={(e) => setContactNumber(e.target.value)}/>
+          
+          {contactError && <span className="text-xs text-red-600">{contactError}</span>}
+          </div>
+
+          
             {/* Password */}
-            <div>
-              <label>Password</label>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
+          <div>
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onFocus={()=>{
+              setPassError("");
+            }}  
+            onChange={(e) => setPassword(e.target.value)}/>
+            {passError && <span className="text-xs text-red-600">{passError}</span>}
+          </div>
+          
 
             {/* Role */}
             <div>
@@ -158,24 +239,35 @@ const Signup = () => {
                   <input
                     type="text"
                     value={specialization}
+                    onFocus={()=>{
+                      setSpecialError("");
+                    }}
                     onChange={(e) => setSpecialization(e.target.value)}
                   />
+                  {specialError && <span className="text-xs text-red-600">{specialError}</span>}
                 </div>
                 <div>
                   <label>Availability</label>
                   <input
                     type="text"
                     value={availability}
-                    onChange={(e) => setAvailability(e.target.value)}
-                  />
-                </div>
+                    onFocus={()=>{
+                      setAvailabilityError("");
+                    }}
+                    onChange={(e) => setAvailability(e.target.value)}/>
+                  </div>
+                  {availableError && <span className="text-xs text-red-600">{availableError}</span>}
                 <div>
                   <label>Fees</label>
                   <input
                     type="number"
+                    onFocus={()=>{
+                      setFeeError("");
+                    }}
                     value={fees}
                     onChange={(e) => setFees(e.target.value)}
                   />
+                  {feeError && <span className="text-xs text-red-600">{feeError}</span>}
                 </div>
               </>
             )}
@@ -186,13 +278,17 @@ const Signup = () => {
               <input
                 type="text"
                 value={address}
+                onFocus={()=>{
+                  setAddError("");
+                }} 
                 onChange={(e) => setAddress(e.target.value)}
-                required
+                
               />
+                {addError && <span className="text-xs text-red-600">{addError}</span>}
             </div>
 
             {/* Submit Button */}
-            <button type="submit" disabled={loading}>
+            <button type="submit" disabled={loading} className="bg-blue-600">
               {loading ? "Signing Up..." : "Signup"}
             </button>
           </form>
