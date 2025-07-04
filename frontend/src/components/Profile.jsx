a9d5e3e1d08ae18b658713408461d717c55714a8
soniyakada <soniyakada123@gmail.com>
2025-07-04 06:24:04 +0530

    Profile UI fixed

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -40,13 +40,15 @@ const Profile = () => {
 
   return (
     <>
-     <div className="flex justify-center items-center h-screen">
+   
      {errorMessage && (
+        <div className="flex justify-center items-center h-screen">
            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4 text-center" role="alert">
              <strong className="font-semibold">Oops!</strong> {errorMessage}
            </div>
+            </div> 
          )}
-        </div> 
+       
       {userDetails ? (
         <div className="">
           {/* Doctor Filter Section */}
